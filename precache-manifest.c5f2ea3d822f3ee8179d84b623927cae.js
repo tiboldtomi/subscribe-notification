2096f4ccdc3e91a619feed8781f10dd4e3cb5ee9
Tamás Tibold <tiboldtomi94@gmail.com>
2020-01-31 00:37:25 +0100

    Updates

---
+++ precache-manifest.c5f2ea3d822f3ee8179d84b623927cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4701ad858b17b8a6cb8179305f9954",
+    "url": "/subscribe-notification/index.html"
+  },
+  {
+    "revision": "9134205ce1bb341a0390",
+    "url": "/subscribe-notification/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9134205ce1bb341a0390",
+    "url": "/subscribe-notification/static/js/2.bc86a7d7.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/subscribe-notification/static/js/2.bc86a7d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a4ef4983c5e8f497b08",
+    "url": "/subscribe-notification/static/js/main.8d1aca55.chunk.js"
+  },
+  {
+    "revision": "c27b1068d52b235dc7e0",
+    "url": "/subscribe-notification/static/js/runtime-main.7f48002f.js"
+  }
+]);
